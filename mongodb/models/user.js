f3daf2300500940048cb1ece724693202d9df848
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-26 12:29:50 +0530

    Added timestamp

--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  avatar: { type: String, required: true },
-  allProperties: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }],
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: { type: String, required: true },
+    allProperties: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }],
+  },
+  { timestamps: true }
+);
 
-const userModel = mongoose.model('User', UserSchema);
+const userModel = mongoose.model("User", UserSchema);
 
 export default userModel;
