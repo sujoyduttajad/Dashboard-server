d2f5feab42c4e9fc760406585f04ce0329391ec0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-26 12:29:52 +0530

    Update property.js

--- mongodb/models/property.js
+++ mongodb/models/property.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const PropertySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  // detailType: { type: String, required: false },
-  description: { type: String, required: true },
-  propertyType: { type: String, required: true },
-  location: { type: String, required: true },
-  price: { type: Number, required: true },
-  photo: { type: String, required: true },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+const PropertySchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    // detailType: { type: String, required: false },
+    description: { type: String, required: true },
+    propertyType: { type: String, required: true },
+    location: { type: String, required: true },
+    price: { type: Number, required: true },
+    photo: { type: String, required: true },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
-
-const propertyModel = mongoose.model('Property', PropertySchema);
+const propertyModel = mongoose.model("Property", PropertySchema);
 
 export default propertyModel;
