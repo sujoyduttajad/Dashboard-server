7e88edc74408e055a0ba5ea6d3deb52931b515a8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-06 23:31:48 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,12 +5,13 @@ import User from "../mongodb/models/user.js";
 */
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({}).limit(req.query._end);
+    const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 /* 
   this block of code creates a new user
