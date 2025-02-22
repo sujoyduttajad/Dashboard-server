b8e4a5ff09e42a06c0f4d23ef99e1dba177fa692
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2025-02-22 01:52:47 -0700

    Update review.routes.js

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createReviews, getAllReviews } from "../controllers/reviews.controller.js";
+import {
+  createReviews,
+  getAllReviews,
+} from "../controllers/reviews.controller.js";
 
 // instance of the express Router
 const router = express.Router();
